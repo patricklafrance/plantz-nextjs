8df12e37987a15ab79f2b5a5c4b969fec7e48088
patricklafrance <pa.lafrance@gmail.com>
2022-03-14 19:09:15 -0400

    Fixed gate

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -20,61 +20,62 @@ function isApiRoute(req: NextRequest) {
 }
 
 async function trialExpiredGate(req: NextRequest) {
-    if (req.nextUrl.pathname.startsWith(TrialExpiredRoute)) {
-        console.log("**** Is trial expired route");
+    if (!req.nextUrl.pathname.startsWith(TrialExpiredRoute)) {
+        const _req = req as NextRequest & { nextauth: { token: JWT } };
 
-        return null;
-    }
+        const fetchUrl = buildUrl(`${getBaseUrl(req)}/api/account/getLicensingStatus`, {
+            id: _req.nextauth.token.userId
+        });
 
-    const _req = req as NextRequest & { nextauth: { token: JWT } };
+        const response = await httpGet<ApiGetResponse<LicensingStatus>>(fetchUrl);
 
-    const fetchUrl = buildUrl(`${getBaseUrl(req)}/api/account/getLicensingStatus`, {
-        id: _req.nextauth.token.userId
-    });
+        if (response.data?.data === "trial-expired") {
+            const url = _req.nextUrl.clone();
+            url.pathname = TrialExpiredRoute;
 
-    const response = await httpGet<ApiGetResponse<LicensingStatus>>(fetchUrl);
-
-    console.log(`Retrieved license status at ${fetchUrl} and receive response: ${response.data?.data}`);
-
-    if (response.data?.data === "trial-expired") {
-        const url = _req.nextUrl.clone();
-        url.pathname = TrialExpiredRoute;
-
-        return NextResponse.redirect(url);
-    }
-
-    return null;
-}
-
-async function middleware(req: NextRequest) {
-    console.log("**** In root middleware");
-
-    if (isPublicRoute(req.nextUrl.pathname) || isApiRoute(req)) {
-        console.log("**** Is public or API route");
-
-        return NextResponse.next();
-    }
-
-    const response = trialExpiredGate(req);
-
-    if (!isNil(response)) {
-        return response;
+            return NextResponse.redirect(url);
+        }
     }
 
     return NextResponse.next();
 }
 
-export default withAuth(
-    middleware,
-    {
-        callbacks: {
-            authorized: ({ req, token }) => {
-                if (isPublicRoute(req.nextUrl.pathname)) {
-                    return true;
-                }
+async function middleware(req: NextRequest) {
+    let response = NextResponse.next();
 
-                return !isNil(token);
+    if (!isPublicRoute(req.nextUrl.pathname) && !isApiRoute(req)) {
+        response = await trialExpiredGate(req);
+    }
+
+    // See https://github.com/vercel/next.js/pull/32767
+    response.headers.set("x-middleware-cache", "no-cache");
+
+    return response;
+}
+
+export default withAuth({
+    callbacks: {
+        authorized: ({ req, token }) => {
+            if (isPublicRoute(req.nextUrl.pathname)) {
+                return true;
             }
+
+            return !isNil(token);
         }
     }
-);
+});
+
+// export default withAuth(
+//     middleware,
+//     {
+//         callbacks: {
+//             authorized: ({ req, token }) => {
+//                 if (isPublicRoute(req.nextUrl.pathname)) {
+//                     return true;
+//                 }
+
+//                 return !isNil(token);
+//             }
+//         }
+//     }
+// );
